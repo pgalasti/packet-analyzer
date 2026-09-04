6af99c131cabd90aa4e6204d4b7874717bc1231d
Paul Galasti <pgalasti@gmail.com>
2026-09-04 18:47:32 -0400

    Playing with lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,54 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <stdexcept>
 
 #include <pcap.h>
 
 constexpr int LOOKUP_OP_FAILURE {-1};
 
+struct ActiveDevice {
+  std::string DeviceName;
+  std::string ReadableAddress;
+};
+
+std::vector<ActiveDevice> ListDevices(); 
+
 int main(int argc, char* argv[]) {
 
-  char szErrorBuffer[PCAP_ERRBUF_SIZE];
-  char* pDevice { pcap_lookupdev(szErrorBuffer) };
+  auto devices {ListDevices()};
 
-  if(pDevice == nullptr) {
-    std::cerr << "Error finding device: " << szErrorBuffer << std::endl;
-    return EXIT_FAILURE;
+  for(auto device : devices) {
+    std::cout << device.DeviceName << std::endl;
   }
 
-  std::cout << "Network device detected: " << pDevice << std::endl;
-
-  bpf_u_int32 addressRaw;
-  bpf_u_int32 mask;
-  auto lookupOp {pcap_lookupnet(pDevice, &addressRaw, &mask, szErrorBuffer) };
-  if(lookupOp == LOOKUP_OP_FAILURE) {
-    std::cerr << "Error lookup address for device: " << szErrorBuffer << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  in_addr address;
-  address.s_addr = addressRaw;
-  char* strAdd {inet_ntoa(address)};
-  if(strAdd == nullptr) {
-    std::cerr << "Error fetching readable address inet_ntoa" << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  std::cout << "Address: " << strAdd << std::endl;
-  
   return 0;
 
+}
+
+
+std::vector<ActiveDevice> ListDevices() {
+  std::vector<ActiveDevice> devices;
+  
+  char szErrorBuffer[PCAP_ERRBUF_SIZE];
+  pcap_if_t* pAllDevs;
+
+  auto opResponse = pcap_findalldevs(&pAllDevs, szErrorBuffer);
+  if(opResponse == LOOKUP_OP_FAILURE || pAllDevs == nullptr) { 
+    throw std::runtime_error("Unable to determine network devices!");
+  }
+ 
+  auto GetDeviceDetails = [](pcap_if_t* pAllDevs) -> ActiveDevice {
+    char szDeviceName[128];
+    strncpy(szDeviceName, pAllDevs->name, 127);
+    szDeviceName[127] = '\0';
+    return {std::string(szDeviceName), ""};
+  };
+
+  for(auto pDev{pAllDevs}; pDev != nullptr; pDev = pDev->next) {
+    devices.push_back(GetDeviceDetails(pDev));
+  }
+
+  return devices; 
 }
